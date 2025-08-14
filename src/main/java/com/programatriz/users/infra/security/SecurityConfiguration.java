3c71ae001343e11f468006fa5044e837fc23b6cb
Silvia Gomes <silvia.gomes@extreme.digital>
2025-08-14 15:01:57 -0300

    refactor: move security dir

--- src/main/java/com/programatriz/users/infra/security/SecurityConfiguration.java
+++ src/main/java/com/programatriz/users/infra/security/SecurityConfiguration.java
@@ -1,4 +1,4 @@
-package com.programatriz.users.infra.cache.security;
+package com.programatriz.users.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
